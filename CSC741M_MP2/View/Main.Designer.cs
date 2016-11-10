4c6e41d7106a85d9110014f6c6bb8338239ca943
Evan Livelo <evan.livelo@gmail.com>
2016-11-11 03:22:39 +0800

    Initial Commit with Initial UI

---
+++ CSC741M_MP2/View/Main.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CSC741M_MP2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.filePathLabel = new MetroFramework.Controls.MetroLabel();
+            this.browseButton = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroTabs = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.fileTypeComboBox = new MetroFramework.Controls.MetroComboBox();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.metroTabs.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // filePathLabel
+            // 
+            this.filePathLabel.Location = new System.Drawing.Point(200, 7);
+            this.filePathLabel.Name = "filePathLabel";
+            this.filePathLabel.Size = new System.Drawing.Size(468, 29);
+            this.filePathLabel.TabIndex = 0;
+            this.filePathLabel.Text = "Click Browse to look for file/folder";
+            this.filePathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(108, 7);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(86, 29);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.AutoScroll = true;
+            this.metroPanel1.HorizontalScrollbar = true;
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(3, 42);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(665, 178);
+            this.metroPanel1.TabIndex = 2;
+            this.metroPanel1.VerticalScrollbar = true;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroTabs
+            // 
+            this.metroTabs.Controls.Add(this.metroTabPage1);
+            this.metroTabs.Controls.Add(this.metroTabPage2);
+            this.metroTabs.Controls.Add(this.metroTabPage3);
+            this.metroTabs.Location = new System.Drawing.Point(23, 63);
+            this.metroTabs.Name = "metroTabs";
+            this.metroTabs.SelectedIndex = 2;
+            this.metroTabs.Size = new System.Drawing.Size(679, 262);
+            this.metroTabs.TabIndex = 3;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.fileTypeComboBox);
+            this.metroTabPage1.Controls.Add(this.metroPanel1);
+            this.metroTabPage1.Controls.Add(this.filePathLabel);
+            this.metroTabPage1.Controls.Add(this.browseButton);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(671, 223);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Home";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            // 
+            // fileTypeComboBox
+            // 
+            this.fileTypeComboBox.FormattingEnabled = true;
+            this.fileTypeComboBox.ItemHeight = 23;
+            this.fileTypeComboBox.Items.AddRange(new object[] {
+            "MPG",
+            "JPG"});
+            this.fileTypeComboBox.Location = new System.Drawing.Point(3, 7);
+            this.fileTypeComboBox.Name = "fileTypeComboBox";
+            this.fileTypeComboBox.Size = new System.Drawing.Size(99, 29);
+            this.fileTypeComboBox.TabIndex = 3;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(671, 223);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Settings";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.Controls.Add(this.metroLabel1);
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.Size = new System.Drawing.Size(671, 223);
+            this.metroTabPage3.TabIndex = 2;
+            this.metroTabPage3.Text = "About";
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 29);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(510, 95);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "CSC741M MP2\r\n\r\nDeveloped and Implemented by: LIVELO, Evan Dennison S. and VER, An" +
+    "drea Nicole O.\r\nSubmitted to: Dr. Conrado Ruiz Jr.\r\n";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 348);
+            this.Controls.Add(this.metroTabs);
+            this.Name = "Main";
+            this.Text = "Video Segmentor";
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.metroTabs.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage3.ResumeLayout(false);
+            this.metroTabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroButton browseButton;
+        private MetroFramework.Controls.MetroLabel filePathLabel;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTabControl metroTabs;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private MetroFramework.Controls.MetroComboBox fileTypeComboBox;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
---
+++ CSC741M_MP2/View/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.IO;
+
+namespace CSC741M_MP2
+{
+    public partial class Main : MetroForm
+    {
+        private string currentPath;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //Property Initialization
+            currentPath = @"C:\";
+
+            //UI Initialization
+            fileTypeComboBox.SelectedIndex = 0;
+            metroTabs.SelectedIndex = 0;
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            if (fileTypeComboBox.SelectedIndex == 0)
+            {
+                OpenFileDialog browser = new OpenFileDialog();
+                browser.Title = "Open Query Video";
+                browser.Filter = "Query Video Files|*.mpg";
+                browser.InitialDirectory = @"C:\";
+                browser.CheckFileExists = true;
+
+                if (browser.ShowDialog() == DialogResult.OK)
+                {
+                    currentPath = browser.FileName.ToString();
+                    filePathLabel.Text = currentPath;
+                }
+            }
+            else if (fileTypeComboBox.SelectedIndex == 1)
+            {
+                FolderBrowserDialog browser = new FolderBrowserDialog();
+                browser.Description = "Set Motion JPEG Path";
+                browser.SelectedPath = @"C:\";
+                browser.ShowNewFolderButton = false;
+
+                if (browser.ShowDialog() == DialogResult.OK)
+                {
+                    currentPath = browser.SelectedPath.ToString();
+                    if (!currentPath.EndsWith("\\")) currentPath = currentPath + "\\";
+                    filePathLabel.Text = currentPath;
+                }
+            }
+        }
+    }
+}
