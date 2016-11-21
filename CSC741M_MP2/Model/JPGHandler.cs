299242858fd99bc9894a20cd8e9a999b3eeb0dae
Evan Livelo <evan.livelo@gmail.com>
2016-11-22 02:22:56 +0800

    Added JPGHandler and IMainView

---
+++ CSC741M_MP2/Model/JPGHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC741M_MP2.Model
+{
+    public class JPGHandler
+    {
+        public delegate void ProgressUpdateEvent(int progress);
+        public event ProgressUpdateEvent ProgressUpdate;
+
+        private string path;
+        private List<String> shotBoundaryPaths;
+        private List<String> keyframePaths;
+
+        public JPGHandler(string path)
+        {
+            this.path = path;
+            shotBoundaryPaths = new List<String>();
+            keyframePaths = new List<String>();
+        }
+
+        public List<String> getShotBoundaries()
+        {
+            // Implementation here.
+            shotBoundaryPaths = Directory.GetFiles(path).Where(p => p.EndsWith(".jpg") || p.EndsWith(".jpeg")).Take(10).ToList();
+            ProgressUpdate(100);
+            return shotBoundaryPaths;
+        }
+
+        public List<String> getKeyframes()
+        {
+            // Implementation here.
+            keyframePaths = Directory.GetFiles(path).Where(p => p.EndsWith(".jpg") || p.EndsWith(".jpeg")).Take(10).ToList();
+            ProgressUpdate(100);
+            return keyframePaths;
+        }
+    }
+}
---
+++ CSC741M_MP2/View/IMainView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC741M_MP2.View
+{
+    public interface IMainView
+    {
+        void openMPGDialog();
+        void openJPGDialog();
+        void showInvalidInputError(string title, string text);
+        void updateProgressBar(int progress);
+        void setUIEnabled(bool enabled);
+        void populateSettings(string defaultSearchPath);
+        void fillShotBoundaryPanel(List<string> shotBoundaries);
+        void fillKeyframePanel(List<string> keyframes);
+    }
+}
